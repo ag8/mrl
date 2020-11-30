03ff0a05dcc00bdd582dc55c803252fc4e4cc964
ag8 <andrew2000g@gmail.com>
2020-11-29 19:04:04 -0500

    current code

--- experiments/sorb/distance_learning.py
+++ experiments/sorb/distance_learning.py
@@ -1,10 +1,8 @@
-import imageio
-
 from envs.goalgridworld.goal_grid import GoalGridWorldEnv
 from mrl.configs.make_discrete_agents import *
 
 
-def goalgridworld_env(grid_file='room_3x3_empty.txt'):
+def goalgridworld_env(grid_file='room_5x5_empty.txt'):
     """
     This is a custom environment function that creates the GoalGridWorld environment.
 
@@ -12,8 +10,8 @@ def goalgridworld_env(grid_file='room_3x3_empty.txt'):
     :return: the gym.GoalEnv environment
     """
     random_init_loc = False
-    random_goal_loc = False
-    env = GoalGridWorldEnv(grid_size=5, max_step=25, grid_file=grid_file, random_init_loc=random_init_loc,
+    random_goal_loc = True
+    env = GoalGridWorldEnv(grid_size=5, max_step=20, grid_file=grid_file, random_init_loc=random_init_loc,
                            random_goal_loc=random_goal_loc)
 
     return env
@@ -22,23 +20,23 @@ def goalgridworld_env(grid_file='room_3x3_empty.txt'):
 def test_sorb():
     print("Running SoRB!")
 
-    max_episode_steps = 5
+    max_episode_steps = 20
 
     print("Making agent...")
     config = get_distance_test_agent_config(args=Namespace(
         env=goalgridworld_env,  # load the GoalGridWorld environment
         parent_folder='/tmp/mrl',  # where to store the logs
-        layers=(74,),  # shape of net
+        layers=(256, 256),  # shape of net
         max_episode_steps=max_episode_steps,  # maximum steps per episode
-        batch_size=80,  # the batch size
-        warm_up=500,  # how many steps to take randomly in order to fill up the replay buffer
-        initial_explore=500,  # bugbug how is this distinct from the previous parameter?
+        batch_size=64,  # the batch size
+        warm_up=1000,  # how many steps to take randomly in order to fill up the replay buffer
+        initial_explore=1000,  # bugbug how is this distinct from the previous parameter?
         num_envs=1,  # number of training environments
         num_eval_envs=1,  # number of testing environments
         use_distributional_rl=True,  # whether to use distributional RL (if false, it will just use the clipping trick)
         target_network_update_freq=5,
         log_every=1000,
-        qvalue_lr=1e-4,
+        qvalue_lr=3e-4,
         optimize_every=1,
         gamma=1
 
@@ -50,7 +48,7 @@ def test_sorb():
 
     t = time.time()
     print("Training agent...")
-    agent.train(num_steps=30000, render=False)
+    agent.train(num_steps=3000000, render=False)
 
     # png_dir = '.'
     # images = []
